f4a126f5d44079f0e434c7cd250e77dcc85ddb40
Admir Demiraj <admdemiraj@gmail.com>
2018-03-08 04:11:40 +0200

    made the project pip installable

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+import sample
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+   
+    name='translation_augmentation',  
+    version=sample.__version__,
+    description='A python project that augments sentences using neural machine translation.',
+    url='https://github.com/admdemiraj/Text_Augmentation_via_Translation',
+    python_requires='>=3',
+    author='Admir Demiraj',
+    license='MIT',
+    author_email='admdemiraj@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='data augmentation nlp machine learning',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+   
+)
